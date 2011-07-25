82ef247287ddfeecf4c2c874a3b14c96e466ec38
Mathias Bynens <mathias@qiwi.be>
2011-07-25 21:55:14 +0200

    Only prevent the default event action if the anchor actually exists.

--- jquery.smoothscroll.js
+++ jquery.smoothscroll.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/smoothscroll v1.5.0 by @mathias */
+/*! http://mths.be/smoothscroll v1.5.1 by @mathias */
 ;(function(document, $) {
 
 	var $scrollElement = (function() {
@@ -24,16 +24,17 @@
 		speed = ~~speed || 400;
 		// Look for links to anchors (on any page)
 		return this.find('a[href*="#"]').click(function(event) {
-			var $hash = $(this.hash); // The in-document element the link points to
-			// Don’t jump to the link right away
-			event.preventDefault();
+			var hash = this.hash,
+			    $hash = $(hash); // The in-document element the link points to
 			// If it’s a link to an anchor in the same document
 			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 				// If the anchor actually exists…
 				if ($hash.length) {
-					// …smoothly scroll to it
-					$scrollElement.stop().animate({ scrollTop: $hash.offset().top }, speed, function() {
-						location.hash = $hash.attr('id');
+					// …don’t jump to the link right away…
+					event.preventDefault();
+					// …and smoothly scroll to it
+					$scrollElement.stop().animate({ 'scrollTop': $hash.offset().top }, speed, function() {
+						location.hash = hash.id;
 					});
 				}
 			}
